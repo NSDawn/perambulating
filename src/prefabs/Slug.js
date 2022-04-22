d714729b1ba99315f39a81fafffb63f3b82e5ef5
TheRealVincentio <StephStyffe@gmail.com>
2022-04-21 21:06:21 -0700

    movement and stops on right.

--- src/prefabs/Slug.js
+++ src/prefabs/Slug.js
@@ -4,6 +4,32 @@ class Slug extends Phaser.GameObjects.Sprite {
         super(scene, x, y, texture, frame);
 
         //add slug to existing scene
-        scene.add.existing(this);
+        scene.add.existing(this); //add to existing, displayList, updateList
+        this.moveSpeed = 0; //movespeed for up or down pixels per frame
+        this.speedMax = 3;
+        this.speedRate = 0.01;
+
+    }
+
+    update(stopped){
+        if(!stopped){
+        if(keyUP.isDown && this.y >= this.height + 32){
+            if(this.moveSpeed<this.speedMax){
+                this.moveSpeed+= this.speedRate;
+            }
+            this.speedRate+= 0.02;
+            this.y -= this.moveSpeed;
+        } else if (keyDOWN.isDown && this.y <= game.config.height -this.height - 32){
+            if(this.moveSpeed<this.speedMax){
+                this.moveSpeed+= this.speedRate;
+            }
+            this.speedRate+= 0.02;
+            this.y += this.moveSpeed;
+        }
+        if(Phaser.Input.Keyboard.JustUp(keyUP)||Phaser.Input.Keyboard.JustUp(keyDOWN)){
+            this.moveSpeed = 0;
+            this.speedRate = 0.01;
+        }
+    }
     }
 }
--- src/scenes/Play.js
+++ src/scenes/Play.js
@@ -12,5 +12,19 @@ class Play extends Phaser.Scene {
         this.add.text(20,20, "oh wow");
         this.playerSlug = new Slug(this, game.config.width-32
             , game.config.height/2, 'slug').setOrigin(0,-0.5);
+        keyDOWN = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.DOWN);
+        keyUP   = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.UP);
+        keyRIGHT= this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.RIGHT);
+        KeyLEFT = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.LEFT);
+        stopped = false;
+    }
+
+    update(){
+        if(keyRIGHT.isDown){
+            stopped = true;
+        }else{
+            stopped = false;
+        }
+        this.playerSlug.update(stopped);
     }
 }
