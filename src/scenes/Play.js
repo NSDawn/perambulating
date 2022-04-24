02310f3461fc1bdaea645585ccc681844e5c0a6d
TheRealVincentio <StephStyffe@gmail.com>
2022-04-23 17:03:15 -0700

    things move in rows now :)

--- src/scenes/Play.js
+++ src/scenes/Play.js
@@ -12,11 +12,27 @@ class Play extends Phaser.Scene {
         this.add.text(20,20, "oh wow");
         this.playerSlug = new Slug(this, game.config.width-32
             , game.config.height/2, 'slug').setOrigin(0,-0.5); // add slug to the scene
-        this.Mushroom1 = new Statics(this, 0, 0, 'slug',0,0).setOrigin(0,0.5);
-        this.Mushroom2 = new Statics(this, 0, game.config.height/5, 'slug',0,1).setOrigin(0,0.5);
-        this.Mushroom3 = new Statics(this, 0, 2*game.config.height/5, 'slug',0,2).setOrigin(0,0.5);
-        this.Mushroom4 = new Statics(this, 0, 3*game.config.height/5, 'slug',0,3).setOrigin(0,0.5);
-        this.Mushroom5 = new Statics(this, 0, 4*game.config.height/5, 'slug',0,4).setOrigin(0,0.5);
+    
+        this.row1= [];
+        for (let index = 0; index < 5; index++) {
+            this.row1[index] = new Statics(this, 0, index*game.config.height/5 + 32, 'slug',0,index).setOrigin(0,0.5);
+        }
+        this.row2= [];
+        for (let index = 0; index < 5; index++) {
+            this.row2[index] = new Statics(this, -game.config.width/5, index*game.config.height/5 + 32, 'slug',0,index).setOrigin(0,0.5);
+        }
+        this.row3= [];
+        for (let index = 0; index < 5; index++) {
+            this.row3[index] = new Statics(this, 2*-game.config.width/5, index*game.config.height/5 + 32, 'slug',0,index).setOrigin(0,0.5);
+        }
+        this.row4= [];
+        for (let index = 0; index < 5; index++) {
+            this.row4[index] = new Statics(this, 3*-game.config.width/5, index*game.config.height/5 + 32, 'slug',0,index).setOrigin(0,0.5);
+        }
+        this.row5= [];
+        for (let index = 0; index < 5; index++) {
+            this.row5[index] = new Statics(this, 4*-game.config.width/5, index*game.config.height/5 + 32, 'slug',0,index).setOrigin(0,0.5);
+        }
         keyDOWN = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.DOWN); //declare keys
         keyUP   = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.UP);
         keyRIGHT= this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.RIGHT);
@@ -37,10 +53,20 @@ class Play extends Phaser.Scene {
             fast = false; //don't if it's is not
         }
         this.playerSlug.update(stopped); //run slug update function
-        this.Mushroom1.update(stopped,fast); //run statics update
-        this.Mushroom2.update(stopped,fast); //run statics update
-        this.Mushroom3.update(stopped,fast); //run statics update
-        this.Mushroom4.update(stopped,fast); //run statics update
-        this.Mushroom5.update(stopped,fast); //run statics update
+        this.row1.forEach( element => {
+            element.update(stopped,fast);
+        });
+        this.row2.forEach( element => {
+            element.update(stopped,fast);
+        });
+        this.row3.forEach( element => {
+            element.update(stopped,fast);
+        });
+        this.row4.forEach( element => {
+            element.update(stopped,fast);
+        });
+        this.row5.forEach( element => {
+            element.update(stopped,fast);
+        });
     }
 }
