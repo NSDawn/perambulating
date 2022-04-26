c8f588f78065f36884124dced563ca20da8f3067
TheRealVincentio <StephStyffe@gmail.com>
2022-04-25 17:30:33 -0700

    implemented movement changes.

--- src/prefabs/Slug.js
+++ src/prefabs/Slug.js
@@ -1,3 +1,26 @@
+//const declaration
+const X_MAX_VELOCITY = 0;
+const X_ACC = 0;
+const X_DEC = 0;
+const LF_BOUND = 0;
+const RT_BOUND = 0;
+
+const Y_MAX_VELOCITY = 4;
+const Y_MAX_VELOCITY_FAST = 6;
+const Y_ACC = 0.1;
+const Y_ACC_FAST = 0.2;
+const Y_DEC = 0.075;
+const Y_DEC_STOPPED = 0.6;
+let UP_BOUND = 0; // reinitialized @super()
+let DN_BOUND = 0; // reinitialized @super()
+
+class v2 {
+    constructor(one,two){
+        this.x = one;
+        this.y = two;
+    }
+}
+
 //slug prefab
 class Slug extends Phaser.GameObjects.Sprite {
     constructor(scene, x, y, texture, frame){
@@ -5,39 +28,60 @@ class Slug extends Phaser.GameObjects.Sprite {
 
         //add slug to existing scene
         scene.add.existing(this); //add to existing, displayList, updateList
-        this.moveSpeed = 0; //movespeed for up or down pixels per frame
-        this.speedMax = 3; //max speed pixels per frame
-        this.speedRate = 0.01; //variable for rate that speed changes per frame
-        this.speedRateplyr = 1.2;
+        this.d = new v2(0, 0); // displacement per frame
+        this.maxd = new v2(X_MAX_VELOCITY, Y_MAX_VELOCITY); // max displacement per frame
+        this.maxdf = new v2(X_MAX_VELOCITY, Y_MAX_VELOCITY_FAST) //max fast displacement
+        this.ACC = new v2(X_ACC, Y_ACC); // acceleration per frame
+        this.ACC_FAST = new v2(X_ACC, Y_ACC_FAST);
+        this.DEC = new v2(X_DEC, Y_DEC);
+        this.DEC_STOPPED = new v2(X_DEC, Y_DEC_STOPPED);
+
+        UP_BOUND = this.height + 32;
+        DN_BOUND = game.config.height - this.height - 32;
 
     }
 
-    update(stopped){
-        if(KeyLEFT.isDown){
-            this.speedMax = 9; 
-            this.speedRateplyr = 2.4;
-         } else if(Phaser.Input.Keyboard.JustUp(KeyLEFT)) {
-             this.speedMax = 3;
-             this.speedRateplyr = 1.2;
-         }
-        if(!stopped){    
-        if(keyUP.isDown && this.y >= this.height + 32){ // if we are not at the top
-            if(this.moveSpeed<this.speedMax){ // and we aren't at max speed
-                this.moveSpeed+= this.speedRate; // increase the speed by the rate
-            }
-            this.speedRate*= this.speedRateplyr; // increase the rate
-            this.y -= this.moveSpeed; // move up by the speed
-        } else if (keyDOWN.isDown && this.y <= game.config.height -this.height - 32){ //else if we aren't at the bottom
-            if(this.moveSpeed<this.speedMax){ //and we aren't at the max speed
-                this.moveSpeed+= this.speedRate; // increase the speed by the rate
-            }
-            this.speedRate*= this.speedRateplyr; // increase the rate
-            this.y += this.moveSpeed; // move down by the speed
+
+    update(stopped, fast){
+        // updating location based on displacement every frame, 
+        //    making sure it does not exceed the bounds
+        if (this.d.x > 0) { // x
+            this.x = Math.max(this.x + this.d.x, RT_BOUND);
+        } else if (this.d.x < 0) {
+            this.x = Math.min(this.x + this.d.x, LF_BOUND);
         }
-        if(Phaser.Input.Keyboard.JustUp(keyUP)||Phaser.Input.Keyboard.JustUp(keyDOWN)){ // after releasing up or down
-            this.moveSpeed = 0; // reset the movespeed
-            this.speedRate = 0.01; // reset the movement rate
+        if (this.d.y < 0) { // y
+            this.y = Math.max(this.y + this.d.y, UP_BOUND);
+        } else if (this.d.y > 0) {
+            this.y = Math.min(this.y + this.d.y, DN_BOUND);
         }
-    }
+        
+         // (x) accelerate if keydown
+        if (!keyDOWN.isDown && keyUP.isDown && !stopped) {
+            this.d.y = Math.max(this.d.y - this.ACC.y, - this.maxd.y);
+        } else if (!keyUP.isDown && keyDOWN.isDown && !stopped) {
+            this.d.y = Math.min(this.d.y + this.ACC.y, this.maxd.y);
+        } else if (!keyDOWN.isDown && keyUP.isDown && !stopped && fast ) {
+            this.d.y = Math.max(this.d.y - this.ACC_FAST.y, - this.maxdf.y);
+        } else if (!keyUP.isDown && keyDOWN.isDown && !stopped && fast) {
+            this.d.y = Math.min(this.d.y + this.ACC_FAST.y, this.maxdf.y);
+        } else if(!stopped){             // decelerate otherwise
+            if (this.d.y > this.DEC.y) {
+                this.d.y -= this.DEC.y
+            } else if (this.d.y < -this.DEC.y) {
+                this.d.y += this.DEC.y;    
+            } else {
+                this.d.y = 0;
+            } 
+        }else{
+            if (this.d.y > this.DEC_STOPPED.y) {
+                this.d.y -= this.DEC_STOPPED.y
+            } else if (this.d.y < -this.DEC_STOPPED.y) {
+                this.d.y += this.DEC_STOPPED.y;    
+            } else {
+                this.d.y = 0;
+            } 
+        }
+        // (y) acceleration would go here 
     }
 }
